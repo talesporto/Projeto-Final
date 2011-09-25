50bcceaa2005771a5aec223611e3b91f20dca00c
Tales Porto <talesap@gmail.com>
2011-09-25 12:47:07 -0300

    Criando parte sobre mahalanobis

--- linuxCompile.sh
+++ linuxCompile.sh
@@ -1,6 +1,6 @@
 pdflatex -output-directory ./bin monografia.tex 
-bibtex bin/monografia
-pdflatex -output-directory ./bin monografia.tex 
-bibtex bin/monografia
-pdflatex -output-directory ./bin monografia.tex 
+#bibtex bin/monografia
+#pdflatex -output-directory ./bin monografia.tex 
+#bibtex bin/monografia
+#pdflatex -output-directory ./bin monografia.tex 
 gnome-open bin/monografia.pdf
